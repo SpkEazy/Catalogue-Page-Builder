0f8e0f3cf459dadd010a0fc34235dac13bbfae56
SpkEazy <jokanovicstudio@gmail.com>
2026-01-30 12:06:18 +0200

    Update builder.js

--- js/builder.js
+++ js/builder.js
@@ -35,6 +35,106 @@ const BROKERS = {
     brokerPhotoCatalogue: "assets/brokers/alex-krause/catalogue.jpg",
     brokerPhotoPresentation: "assets/brokers/alex-krause/presentation.png"
   },
+
+  "bongane-khumalo": {
+    displayName: "Bongane Khumalo",
+    brokerName: "BONGANE KHUMALO",
+    brokerPhone: "073 785 5100",
+    brokerEmail: "bongane@auctioninc.co.za",
+    brokerPhotoCatalogue: "assets/brokers/bongane-khumalo/catalogue.jpg",
+    brokerPhotoPresentation: "assets/brokers/bongane-khumalo/presentation.png"
+  },
+
+  "cliff-matshatsha": {
+    displayName: "Cliff Matshatsha",
+    brokerName: "CLIFF MATSHATSHA",
+    brokerPhone: "082 099 8692",
+    brokerEmail: "cliff@auctioninc.co.za",
+    brokerPhotoCatalogue: "assets/brokers/cliff-matshatsha/catalogue.jpg",
+    brokerPhotoPresentation: "assets/brokers/cliff-matshatsha/presentation.png"
+  },
+
+  "daniel-wachenheimer": {
+    displayName: "Daniel Wachenheimer",
+    brokerName: "DANIEL WACHENHEIMER",
+    brokerPhone: "082 740 2856",
+    brokerEmail: "daniel@auctioninc.co.za",
+    brokerPhotoCatalogue: "assets/brokers/daniel-wachenheimer/catalogue.jpg",
+    brokerPhotoPresentation: "assets/brokers/daniel-wachenheimer/presentation.png"
+  },
+
+  "dean-doucha": {
+    displayName: "Dean Doucha",
+    brokerName: "DEAN DOUCHA",
+    brokerPhone: "082 374 5565",
+    brokerEmail: "dean@auctioninc.co.za",
+    brokerPhotoCatalogue: "assets/brokers/dean-doucha/catalogue.jpg",
+    brokerPhotoPresentation: "assets/brokers/dean-doucha/presentation.png"
+  },
+
+  "doron-sacks": {
+    displayName: "Doron Sacks",
+    brokerName: "DORON SACKS",
+    brokerPhone: "082 550 7081",
+    brokerEmail: "doron@auctioninc.co.za",
+    brokerPhotoCatalogue: "assets/brokers/doron-sacks/catalogue.jpg",
+    brokerPhotoPresentation: "assets/brokers/doron-sacks/presentation.png"
+  },
+
+  "elki-medalie": {
+    displayName: "Elki Medalie",
+    brokerName: "ELKI MEDALIE",
+    brokerPhone: "083 764 5370",
+    brokerEmail: "elki@auctioninc.co.za",
+    brokerPhotoCatalogue: "assets/brokers/elki-medalie/catalogue.jpg",
+    brokerPhotoPresentation: "assets/brokers/elki-medalie/presentation.png"
+  },
+
+  "gary-brower": {
+    displayName: "Gary Brower",
+    brokerName: "GARY BROWER",
+    brokerPhone: "082 352 5552",
+    brokerEmail: "garyb@auctioninc.co.za",
+    brokerPhotoCatalogue: "assets/brokers/gary-brower/catalogue.jpg",
+    brokerPhotoPresentation: "assets/brokers/gary-brower/presentation.png"
+  },
+
+  "george-merricks": {
+    displayName: "George Merricks",
+    brokerName: "GEORGE MERRICKS",
+    brokerPhone: "082 859 9303",
+    brokerEmail: "george@auctioninc.co.za",
+    brokerPhotoCatalogue: "assets/brokers/george-merricks/catalogue.jpg",
+    brokerPhotoPresentation: "assets/brokers/george-merricks/presentation.png"
+  },
+
+  "gerhard-venter": {
+    displayName: "Gerhard Venter",
+    brokerName: "GERHARD VENTER",
+    brokerPhone: "076 905 5519",
+    brokerEmail: "gerhard@auctioninc.co.za",
+    brokerPhotoCatalogue: "assets/brokers/gerhard-venter/catalogue.jpg",
+    brokerPhotoPresentation: "assets/brokers/gerhard-venter/presentation.png"
+  },
+
+  "jenny-pillay": {
+    displayName: "Jenny Pillay",
+    brokerName: "JENNY PILLAY",
+    brokerPhone: "063 959 2260",
+    brokerEmail: "jenny@auctioninc.co.za",
+    brokerPhotoCatalogue: "assets/brokers/jenny-pillay/catalogue.jpg",
+    brokerPhotoPresentation: "assets/brokers/jenny-pillay/presentation.png"
+  },
+
+  "jessica-beyers-lahner": {
+    displayName: "Jessica Beyers-Lahner",
+    brokerName: "JESSICA BEYERS-LAHNER",
+    brokerPhone: "072 576 0973",
+    brokerEmail: "jessica@auctioninc.co.za",
+    brokerPhotoCatalogue: "assets/brokers/jessica-beyers-lahner/catalogue.jpg",
+    brokerPhotoPresentation: "assets/brokers/jessica-beyers-lahner/presentation.png"
+  },
+
   "jodi-bedil": {
     displayName: "Jodi Bedil",
     brokerName: "JODI BEDIL",
@@ -43,16 +143,109 @@ const BROKERS = {
     brokerPhotoCatalogue: "assets/brokers/jodi-bedil/catalogue.jpg",
     brokerPhotoPresentation: "assets/brokers/jodi-bedil/presentation.png"
   },
-  "gary-brower": {
-    displayName: "Gary Brower",
-    brokerName: "GARY BROWER",
-    brokerPhone: "",   // fill in
-    brokerEmail: "",   // fill in
-    brokerPhotoCatalogue: "assets/brokers/gary-brower/catalogue.jpg",
-    brokerPhotoPresentation: "assets/brokers/gary-brower/presentation.png"
+
+  "jodi-frankel": {
+    displayName: "Jodi Frankel",
+    brokerName: "JODI FRANKEL",
+    brokerPhone: "082 441 8409",
+    brokerEmail: "jodif@auctioninc.co.za",
+    brokerPhotoCatalogue: "assets/brokers/jodi-frankel/catalogue.jpg",
+    brokerPhotoPresentation: "assets/brokers/jodi-frankel/presentation.png"
+  },
+
+  "keith-nkosi": {
+    displayName: "Keith Nkosi",
+    brokerName: "KEITH NKOSI",
+    brokerPhone: "081 828 1817",
+    brokerEmail: "keith@auctioninc.co.za",
+    brokerPhotoCatalogue: "assets/brokers/keith-nkosi/catalogue.jpg",
+    brokerPhotoPresentation: "assets/brokers/keith-nkosi/presentation.png"
+  },
+
+  "luanda-tlhotlhalemaje": {
+    displayName: "Luanda Tlhotlhalemaje",
+    brokerName: "LUANDA TLHOTLHALEMAJE",
+    brokerPhone: "071 904 4061",
+    brokerEmail: "luanda@skyriseproperties.co.za",
+    brokerPhotoCatalogue: "assets/brokers/luanda-tlhotlhalemaje/catalogue.jpg",
+    brokerPhotoPresentation: "assets/brokers/luanda-tlhotlhalemaje/presentation.png"
+  },
+
+  "nic-brett": {
+    displayName: "Nic Brett",
+    brokerName: "NIC BRETT",
+    brokerPhone: "078 330 7523",
+    brokerEmail: "nic@auctioninc.co.za",
+    brokerPhotoCatalogue: "assets/brokers/nic-brett/catalogue.jpg",
+    brokerPhotoPresentation: "assets/brokers/nic-brett/presentation.png"
+  },
+
+  "reece-louw": {
+    displayName: "Reece Louw",
+    brokerName: "REECE LOUW",
+    brokerPhone: "076 393 1131",
+    brokerEmail: "reece@auctioninc.co.za",
+    brokerPhotoCatalogue: "assets/brokers/reece-louw/catalogue.jpg",
+    brokerPhotoPresentation: "assets/brokers/reece-louw/presentation.png"
+  },
+
+  "reshma-sookran": {
+    displayName: "Reshma Sookran",
+    brokerName: "RESHMA SOOKRAN",
+    brokerPhone: "071 876 6524",
+    brokerEmail: "reshma@auctioninc.co.za",
+    brokerPhotoCatalogue: "assets/brokers/reshma-sookran/catalogue.jpg",
+    brokerPhotoPresentation: "assets/brokers/reshma-sookran/presentation.png"
+  },
+
+  "shlomo-hecht": {
+    displayName: "Shlomo Hecht",
+    brokerName: "SHLOMO HECHT",
+    brokerPhone: "073 791 7967",
+    brokerEmail: "shlomo@auctioninc.co.za",
+    brokerPhotoCatalogue: "assets/brokers/shlomo-hecht/catalogue.jpg",
+    brokerPhotoPresentation: "assets/brokers/shlomo-hecht/presentation.png"
+  },
+
+  "sim-mthembu": {
+    displayName: "Sim Mthembu",
+    brokerName: "SIM MTHEMBU",
+    brokerPhone: "063 829 7431",
+    brokerEmail: "simphiwe@auctioninc.co.za",
+    brokerPhotoCatalogue: "assets/brokers/sim-mthembu/catalogue.jpg",
+    brokerPhotoPresentation: "assets/brokers/sim-mthembu/presentation.png"
+  },
+
+  "stuart-holliman": {
+    displayName: "Stuart Holliman",
+    brokerName: "STUART HOLLIMAN",
+    brokerPhone: "067 373 9239",
+    brokerEmail: "stuart@auctioninc.co.za",
+    brokerPhotoCatalogue: "assets/brokers/stuart-holliman/catalogue.jpg",
+    brokerPhotoPresentation: "assets/brokers/stuart-holliman/presentation.png"
+  },
+
+  "thabani-ncube": {
+    displayName: "Thabani Ncube",
+    brokerName: "THABANI NCUBE",
+    brokerPhone: "071 624 2899",
+    brokerEmail: "thabani@auctioninc.co.za",
+    brokerPhotoCatalogue: "assets/brokers/thabani-ncube/catalogue.jpg",
+    brokerPhotoPresentation: "assets/brokers/thabani-ncube/presentation.png"
+  },
+
+  "yoni-dadon": {
+    displayName: "Yoni Dadon",
+    brokerName: "YONI DADON",
+    brokerPhone: "061 822 6128",
+    brokerEmail: "yoni@auctioninc.co.za",
+    brokerPhotoCatalogue: "assets/brokers/yoni-dadon/catalogue.jpg",
+    brokerPhotoPresentation: "assets/brokers/yoni-dadon/presentation.png"
   }
-  // Add more brokers here...
+
+
 };
+
 
 // Optional: a safe default if someone forgets to choose
 const DEFAULT_BROKER_SLUG = "alex-krause";
@@ -378,4 +571,5 @@ async function generateAndDownload(templateType) {
 
   container.innerHTML = '';
 }
+
 
